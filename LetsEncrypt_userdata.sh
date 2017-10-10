de69c5c6d90ae2a51f1e9e02b04b9d6171a39a4a
James Kwok <jameskwok@L-C02V81BCHV2J-James-Kwok.local>
2017-10-10 18:56:22 +1100

    update

--- LetsEncrypt_userdata.sh
+++ LetsEncrypt_userdata.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-#Install Dependencies
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-apt-get install -y nodejs nginx python awscli ntp
-#Install LetsEncrypt
-add-apt-repository -y ppa:certbot/certbot
-apt-get update
-apt-get install -y python-certbot-nginx
-#Clone repo
+apt-get install -y docker.io socat apt-transport-https htop git
 git clone https://github.com/jamkwok/typescript_sslify.git
-#Npm install
-cd typescript_sslify/app
-npm install
-npm install -g pm2
-npm install -g typescript
-npm install -g gulp
-#compile typescript
-tsc
-#start app
-su - root -c "pm2 start --name sslSentry /typescript_sslify/app/src/app.js"
-echo "0 3 * * * /usr/bin/node /typescript_sslify/app/src/app_scrape.js" > crontab.txt
-crontab crontab.txt
