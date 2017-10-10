92d8ee584c4ae685cdb0b598713330b818be2430
James Kwok <jameskwok@L-C02V81BCHV2J-James-Kwok.local>
2017-10-10 19:16:31 +1100

    up

--- LetsEncrypt_userdata.sh
+++ LetsEncrypt_userdata.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+#Install Docker
 apt-get install -y docker.io socat apt-transport-https htop git
+#Get Source Code
 git clone https://github.com/jamkwok/typescript_sslify.git
+cd typescript_sslify
+#Build Docker Image
+docker build .
